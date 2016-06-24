6a4ca582903cb344367dc815641d2aeecb13bedb
Poy Chang <poypost@gmail.com>
2016-06-24 11:22:35 +0800

    修改側邊欄的搜尋框 ID，並可隨時輸入查詢關鍵字

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
 	/* Sidebar height set */
-	$('.sidebar').css('min-height',$(document).height());
+	$('.sidebar').css('min-height', $(document).height());
 
 	/* Secondary contact links */
 	var scontacts = $('#contact-list-secondary');
 	var contact_list = $('#contact-list');
-	
-	scontacts.hide();
-	
-	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
-	
-	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
 
+	scontacts.hide();
+
+	contact_list.mouseenter(function () { scontacts.fadeIn(); });
+
+	contact_list.mouseleave(function () { scontacts.fadeOut(); });
+	
+	// input keyword in sidebar-search-box when key down
+    document.onkeydown = function () {
+        document.getElementById("sidebar-search-box").focus();
+    };
 });
