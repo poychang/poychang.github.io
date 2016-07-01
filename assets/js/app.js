ac77a5c8ec3a31bb3bda37cf8ae31f30e8a11e9c
Poy Chang <poypost@gmail.com>
2016-07-01 13:48:35 +0800

    修改監聽鍵盤事件的動作

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,14 @@ $(document).ready(function () {
 	contact_list.mouseleave(function () { scontacts.fadeOut(); });
 	
 	// input keyword in sidebar-search-box when key down
-    document.onkeydown = function () {
-        document.getElementById("sidebar-search-box").focus();
+    document.onkeydown = function (event) {
+		if ((event.keyCode >= 65 && event.keyCode <= 90) ||
+			(event.keyCode >= 97 && event.keyCode <= 122)) {
+			// only keyCode is a-z or A-Z
+			document.getElementById("sidebar-search-box").focus();
+		}
+		if (event.keyCode === 27) { // Esc
+			document.getElementById("sidebar-search-box").blur();
+		}
     };
 });
