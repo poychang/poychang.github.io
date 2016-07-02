3a11866d5c84c0ae53039dad66f8ca57c6225b9a
Poy Chang <poypost@gmail.com>
2016-07-02 11:56:09 +0800

    修改監聽鍵盤事件的動作，避免無法執行 ctrl+c 等動作

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,13 +15,17 @@ $(document).ready(function () {
 	
 	// input keyword in sidebar-search-box when key down
     document.onkeydown = function (event) {
-		if ((event.keyCode >= 65 && event.keyCode <= 90) ||
-			(event.keyCode >= 97 && event.keyCode <= 122)) {
-			// only keyCode is a-z or A-Z
-			document.getElementById("sidebar-search-box").focus();
-		}
-		if (event.keyCode === 27) { // Esc
-			document.getElementById("sidebar-search-box").blur();
-		}
+        var azKeySet = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+        var AZKeySet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
+
+        if (!event.altKey && !event.ctrlKey && !event.shiftKey) {
+            if (azKeySet.indexOf(event.keyCode) > -1 || AZKeySet.indexOf(event.keyCode) > -1) {
+                // only keyCode is a-z or A-Z
+                document.getElementById("sidebar-search-box").focus();
+            }
+            if (event.keyCode === 27) { // Esc
+                document.getElementById("sidebar-search-box").blur();
+            }
+        }
     };
 });
