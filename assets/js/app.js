e7ff64f7b2dd06264e14c438b9b76314d60bd473
Poy Chang <poypost@gmail.com>
2016-07-02 11:56:23 +0800

    clean code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
 
-	/* Sidebar height set */
-	$('.sidebar').css('min-height', $(document).height());
+    /* Sidebar height set */
+    $('.sidebar').css('min-height', $(document).height());
 
-	/* Secondary contact links */
-	var scontacts = $('#contact-list-secondary');
-	var contact_list = $('#contact-list');
+    /* Secondary contact links */
+    var scontacts = $('#contact-list-secondary');
+    var contact_list = $('#contact-list');
 
-	scontacts.hide();
+    scontacts.hide();
 
-	contact_list.mouseenter(function () { scontacts.fadeIn(); });
+    contact_list.mouseenter(function () { scontacts.fadeIn(); });
 
-	contact_list.mouseleave(function () { scontacts.fadeOut(); });
-	
-	// input keyword in sidebar-search-box when key down
+    contact_list.mouseleave(function () { scontacts.fadeOut(); });
+
+    // input keyword in sidebar-search-box when key down
     document.onkeydown = function (event) {
         var azKeySet = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
         var AZKeySet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
