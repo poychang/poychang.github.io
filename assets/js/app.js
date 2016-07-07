f1118410ab01bc2938c904b0a49c85bc910c6192
unknown <12258@seec.com.tw>
2016-07-07 09:36:26 +0800

    搜尋偵測加入 metaKey， 主要用於偵測 Mac 的 Command Key

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         var azKeySet = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
         var AZKeySet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
 
-        if (!event.altKey && !event.ctrlKey && !event.shiftKey) {
+        if (!event.altKey && !event.ctrlKey && !event.shiftKey && !event.metaKey) {
             if (azKeySet.indexOf(event.keyCode) > -1 || AZKeySet.indexOf(event.keyCode) > -1) {
                 // only keyCode is a-z or A-Z
                 document.getElementById("sidebar-search-box").focus();
