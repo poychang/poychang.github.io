1117ac8304b9232e7f0733f21415d07d0287f0bb
Poy Chang <poypost@gmail.com>
2018-03-02 18:11:40 +0800

    add post date to search result

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,6 +9,7 @@
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
         appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<small>' + item.date + '...</small>';
         appendString += '<p>' + item.content.substring(0, 100) + '...</p></li>';
       }
 
